384450cd8caf2e4ab4299d2b45dffdfacd7ee12c
ktorresagui <al03022345@tecmilenio.mx>
2026-01-20 15:13:53 -0600

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,13 @@
+// alert("Hola mundo");
+
+function mostrar() {
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+    
+    let resultado = 0;
+    resultado = numero1 + numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = resultado;
+
+}
